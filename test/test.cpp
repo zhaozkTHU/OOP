704789d62363b42971865816e059c9d5b68c8c5d
zhaozk <zhaozk21@outlook.com>
2022-05-19 10:48:52 +0800

    1

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <regex>
+using namespace std;
+
+int a() { return 0; }
+
+int main() {
+	int a[10];
+	sort(a, a + 10);
+}
+
+//dssd
--- tmp/main.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-filename=test
-for par in $@;
-do
-mkdir output-$par && python3 ${filename}.py $par 
-done
