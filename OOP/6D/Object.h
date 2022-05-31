6e6683e1fb11261e7583d623c2703ada7cfb3ac9
zhaozk <zhaozk21@outlook.com>
2022-05-31 11:18:11 +0800

    6D finished

--- OOP/6D/Object.h
+++ OOP/6D/Object.h
@@ -1,86 +1,86 @@
 #pragma once
 #include <list>
 #include <iostream>
+#include <memory>
 
 #include "CustomClass.h"
 
-class Content
-{
-	virtual void output(std::ostream& out){ //virtual function for output
+class Object;
+
+class Content {
+public:
+	virtual void output(std::ostream &out) { //virtual function for output
 		std::cout << "the operator is not supported for this type." << std::endl;
+		out << 1;
 	}
+	virtual void operator+=(int) {}
+	virtual void operator+=(const std::string &) {}
+	virtual Object& operator[](int) {}
 };
 
-class IntContent: public Content
-{
+class IntContent : public Content {
 private:
 	int x;
 public:
-	IntContent(int _x): x(_x) {}
-	void output(const std::ostream& out){ //???
+	IntContent(int _x) : x(_x) {}
+	void output(std::ostream &out) {
 		out << x;
 	}
+	void operator+=(int y) { x += y; }
 };
-class StringContent: public Content
-{
+
+class StringContent : public Content {
 private:
 	std::string x;
 public:
-	StringContent(std::string _x): x(_x) {}
-	void output(const std::ostream& out){  //???
+	StringContent(std::string _x) : x(_x) {}
+	void output(std::ostream &out) {
 		out << x;
 	}
+	void operator+=(const std::string &y) { x += y; }
 };
-// The following codes are not working
-// class VectorContent: public Content
-// {
-// private:
-// 	std::vector<Object> x; //Object is not defined???
-// public:
-// 	VectorContent(const std::vector<Object>& _x): x(_x) {}
-// };
-class CustomContent: public Content
-{
+
+class VectorContent : public Content {
+	std::vector<Object> x;
+public:
+	VectorContent(const std::vector<Object> &_x) : x(_x) {}
+	Object& operator[](int _x) {
+		return x[_x];
+	}
+};
+
+class CustomContent : public Content {
 private:
 	CustomClass x;
 public:
-	CustomContent(const CustomClass& _x): x(_x) {}
+	CustomContent(const CustomClass &_x) : x(_x) {}
 };
 
-class Object
-{
+class Object {
 private:
-	Content* pt;
-
+	//Content *pt;
+	std::shared_ptr<Content> pt;
 public:
 	Object() {}
-	Object(int x) {
-		pt = new int(x); // where to delete? maybe use std::shared_ptr?
-	}
-	Object(const std::string &x){
-		pt = new StringContent(x);
-	}
-	// The following codes are not working
-	// Object(const std::vector<Object> &x){
-	// 	pt = new VectorContent(x);
-	// }
-	Object(const CustomClass &x){
-		pt = new CustomContent(x);
-	}
+	Object(int x) : pt(new IntContent(x)) {}
+	Object(const std::string &x) : pt(new StringContent(x)) {}
+	Object(const std::vector<Object> &x) : pt(new VectorContent(x)) {}
+	Object(const CustomClass &x) : pt(new CustomContent(x)) {}
 
-	friend std::ostream& operator<<(std::ostream& out, const Object& obj) {
+	friend std::ostream &operator<<(std::ostream &out, const Object &obj) {
 		obj.pt->output(out);
 		return out;
 	}
 
-	Object& operator+=(int y){
+	Object &operator+=(int y) {
 		pt->operator+=(y);
 		return *this;
 	}
-	Object& operator+=(const std::string &y){
+	Object &operator+=(const std::string &y) {
 		pt->operator+=(y);
 		return *this;
 	}
-
-	//need more operators......
+	Object &operator[](int y) {
+		return pt->operator[](y);
+	}
 };
